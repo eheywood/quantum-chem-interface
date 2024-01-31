70372dfdc256693647d7e429e6b941359e93993d
Ellie Heywood <ejh3g21@soton.ac.uk>
2024-01-31 16:42:26 +0000

    Laying out more plans and general structures

--- backend/QVM_cirq.py
+++ backend/QVM_cirq.py
@@ -1,1 +1,20 @@
 ## QVM provided by Cirq
+
+class QVM:
+
+    def __init__(self) -> None:
+        # need to specify type, noise/no noise, num of quibits, processor type etc...
+        # default -> load from config file
+        
+        self.qibit_num = None
+
+    #def circuit translator?
+        
+    # def run
+    
+    # def interpret results (image? nums? probabilities?)
+
+    # prepare cicuit
+    
+    # take config file and update QVM
+        
