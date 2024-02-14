04efe6d89a1366770ccc5a8276d100ec3d06e259
Ellie Heywood <ejh3g21@soton.ac.uk>
2024-02-14 16:29:13 +0000

    WIP: Started building outline for Circuit class.

---
+++ backend/Circuit.py
@@ -0,0 +1,8 @@
+class Circuit:
+
+    def __init__(self) -> None:
+        pass
+
+    # def cirq_translate_circuit
+
+    # Need to think about how to store circuit - what kind of details required etc
