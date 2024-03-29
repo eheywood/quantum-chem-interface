9ae3caa9a5ed02b9e3827f097aa9114b38481cb8
Ellie Heywood <ejh3g21@soton.ac.uk>
2024-03-29 16:44:42 +0000

    ADD: Implemented the quantum fourier transform and inverse

--- model/simulations/ParticleInBoxSim.py
+++ model/simulations/ParticleInBoxSim.py
@@ -13,6 +13,7 @@
 import cirq
 import cirq_google
 import qsimcirq
+import numpy as np 
 
 # TODO: def build_circuit(L:float,N=8:int,wave:str):
     #n_quibits = math,log2(N)
@@ -39,10 +40,72 @@ import qsimcirq
 
 # TODO: def interpret_wavefunc():  Takes the wavefunction and returns the initial states of the qubits that is required.
 
-# TODO: def QFT and inv QFT for the number of qubits... 
+def QFT(qubits):
+    moment = []
+    for q in range(len(qubits)):
+        moment.append(cirq.Moment(cirq.H(qubits[q])))
+        count = 2
+        for i in range(q+1,len(qubits)):
+            rk = R_k(count)
+            count +=1
+            moment.append(cirq.Moment(cirq.ControlledGate(rk).on(qubits[i],qubits[q])))
+
+    return moment
+
+def inv_QFT(qubits):
+    moment = []
+    for q in range(len(qubits)-1,-1,-1):
+        count = len(qubits) - q
+        for i in range(len(qubits)-1,q,-1):
+            print(count)
+            rk = R_k_inv(count)
+            count -=1
+            moment.append(cirq.Moment(cirq.ControlledGate(rk).on(qubits[i],qubits[q])))
+        print('H')
+        moment.append(cirq.Moment(cirq.H(qubits[q])))
+    return moment
 
 # TODO: def phaseShift -> for momentum operator as well as 
 
+
+## Custom gates to perform the unitary transform required for the QFT
+class R_k(cirq.Gate):
+    def __init__(self,k):
+        super(R_k, self)
+        self.k = k
+
+
+    def _num_qubits_(self):
+        return 1
+
+    def _unitary_(self):
+        return np.array([
+            [1.0,  0],
+            [0, (np.e **((2 * np.pi * 1.j)/2**self.k))]
+        ])
+
+    def _circuit_diagram_info_(self, args):
+        return "R_" + str(self.k)
+    
+
+class R_k_inv(cirq.Gate):
+    def __init__(self,k):
+        super(R_k, self)
+        self.k = k
+
+
+    def _num_qubits_(self):
+        return 1
+
+    def _unitary_(self):
+        return np.array([
+            [1.0,  0],
+            [0, -(np.e **((2 * np.pi * 1.j)/2**self.k))]
+        ])
+
+    def _circuit_diagram_info_(self, args):
+        return "R_" + str(self.k) + "^+"
+    
 
 
 
