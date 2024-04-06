2ccb4eae469cfaf66a5520423039c34b4ff803d3
Ellie Heywood <ejh3g21@soton.ac.uk>
2024-04-06 12:52:53 +0100

    ADD: Can now verify the circuit before sumbitting the job to IBMQ

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Educational Quantum Computing Interface to aid Teaching Quantum Chemistry
 
 A tool-box to play around with simulations on different quantum computing backends.
+
+
+All circuits must use 'meas' as the register name for measurements...
--- model/IBM_Q_interface.py
+++ model/IBM_Q_interface.py
@@ -1,19 +1,33 @@
-from qiskit import QuantumCircuit
+from qiskit import QuantumCircuit, transpile
 from qiskit.quantum_info import SparsePauliOp
-from qiskit_aer.primitives import Estimator
-from qiskit_ibm_runtime import QiskitRuntimeService
+from qiskit.primitives import BackendSampler
+from qiskit_ibm_runtime import QiskitRuntimeService, SamplerV2 as Sampler
+from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+from qiskit_aer import AerSimulator
 
 class IBM_Q:
 
-    optimisation_lvl = None
-    backend_name = 'ibm-brisbane'
+    optimisation_lvl = 1
+    backend_name = 'ibm_brisbane'
 
+    num_repetitions = 1000
     service = None
+    token = None
+
 
     def __init__(self,token,config=None) -> None:
         
-        self.service = QiskitRuntimeService(channel='ibm_quantum',token=token)
+        try:
+            self.service = QiskitRuntimeService(channel='ibm_quantum',token=token)
+            self.token = token
+            print('Successfully connected to qiskit runtime service')
+            
+        except Exception:
+            raise Exception("Qiskit Runtime service failed. Check token is correct.")
     
+    #def default_set_up(self):
+
+
     def set_up_config(self,config):
 
         for setting, value in config.items():
@@ -26,10 +40,56 @@ class IBM_Q:
                 case _:
                     raise AttributeError(setting + " is not a known key within the configuration file.")
 
+    def check_job_status(self,job_id):
+        """ Checks the status of a job.
+
+        :param job_id: The ID of the job in question.
+        :type job_id: str
+        :raises Exception: If the job id is incorrect,
+        :return: The status of the job.
+        :rtype: JobStatus (Enum)
+        """
+        
+        try:
+            job = self.service(job_id)
+        except:
+            raise Exception("Could not find job. Check job ID")
+        
+        return job.status()
+
+    def get_results_from_job(self,job_id):
+        """ Gets the results from a job submitted to a quantum machine.
+
+        :param job_id: The ID of the job in question.
+        :type job_id: str
+        :raises Exception: If the job ID is incorrect
+        :return: Returns a boolean with if the job completes and the probability distribution of results.
+        :rtype: bool, dict
+        """
+
+        try:
+            job = self.service(job_id)
+        except:
+            raise Exception("Could not find job. Check job ID")
+
+        status = job.status()
+
+        match status.name():
+            case 'DONE':
+                result = job.results()
+                distribution = result.quasi_dist[0].binary_probabilities()
+                complete = True
+
+            case _:
+                complete = False
+                distribution = None
+
+        return complete, distribution
+        
 
 
-    def run(self, circuit:QuantumCircuit):
-        print('Running circuit...')
+    def submit_job(self, circuit:QuantumCircuit):
+        print('Submitting circuit...')
 
         ### Transpile circuit
 
@@ -38,7 +98,34 @@ class IBM_Q:
         ### Run
 
     def verify_via_sim(self, circuit:QuantumCircuit):
+        """ Verifies the circuit works using a simulator. Returns the results from the simulation.
+
+        :param circuit: The circuit to validate.
+        :type circuit: QuantumCircuit
+        :return: A dictionary of the counts from the measurements
+        :rtype: dict
+        """
         print('Verifying circuit...')
 
-        ## Need a circuit without measurements...
+        try:
+            backend = self.service.backend(self.backend_name)
+            simulator = AerSimulator.from_backend(backend)
+
+            circuit.measure_all()
+
+            circ = transpile(circuit,simulator)
+
+            sampler = Sampler(simulator)
+
+            job = sampler.run([circ],shots = self.num_repetitions)
+            
+            #TODO: improve this to remove 'meas' name requirement
+            result = job.result()[0].data.meas.get_counts()
+        except:
+            return None
+        return result
+
+        
+
+
         
