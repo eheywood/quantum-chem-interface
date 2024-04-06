065c8aa8d463b6d28b6beed2cee0a3964f337c04
Ellie Heywood <ejh3g21@soton.ac.uk>
2024-04-06 18:32:45 +0100

    ADD: Began planning the MVC setup

---
+++ view/cmd_line_view.py
@@ -0,0 +1,19 @@
+import curses
+
+class CMD_line_interface:
+
+    stdscr = None
+
+    def __init__(self) -> None:
+        stdsrc = curses.initscr()
+
+
+    # def menu_page()
+    
+
+    # def p_in_box_problem()
+        
+    
+    # Add other problems here...
+    
+
