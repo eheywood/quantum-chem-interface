495495686f9e44581e7411d53b69c449659cf8ca
Ellie Heywood <ejh3g21@soton.ac.uk>
2024-04-07 11:35:32 +0100

    ADD: Basic functionality to load a configuration file

--- controller.py
+++ controller.py
@@ -1,14 +1,18 @@
 from view import cmd_line_view
 from model import *
+import yaml
+import time
 
 class Controller:
       
     view = None
     config = None
 
-    menu_options = ["Particle In a Box", "Exit"]
+    menu_options = ["Particle In a Box", "Load Configuration File", "Exit"]
 
     def __init__(self) -> None:
+
+        ## Initialise View and then go straight to menu
         self.view = cmd_line_view.CMD_line_interface()
         self.menu()
 
@@ -18,21 +22,34 @@ class Controller:
         match self.menu_options[option_index]:
             case "Particle In a Box":
                 print ("Particle In a Box")
+            case "Load Configuration File":
+                  self.get_config_file()
             case "Exit":
                 self.view.close_application()
                 exit()
             
-    #def get_config_file(self):
+    def get_config_file(self):
         ## Get absolute location of file from user
+        path = self.view.get_config_filepath()
 
         ## Open file and save configuration.
+        try:
+            config_file = open(path[:-2],'r')
+            self.config = yaml.safe_load(config_file)
+            msg = "Config file successfully loaded."
+        except:
+            msg = "ERROR: Loading configuration file failed. Check path spelling."
+
+        ## Confirmation message
+        self.view.display_temp_msg(msg)
+        time.sleep(1)
+        
+        self.menu()
 
     #def particle_in_box(self):
          ## Get user input from view.
 
          ## Format data to build circuit
 
-         
-    
 if __name__ == '__main__':
 	controller = Controller()
--- view/cmd_line_view.py
+++ view/cmd_line_view.py
@@ -1,4 +1,5 @@
 import curses
+from curses.textpad import Textbox, rectangle
 
 class CMD_line_interface:
 
@@ -20,7 +21,9 @@ class CMD_line_interface:
 
 
     def menu_page(self, menu_options):
-
+        
+        self.stdscr.clear()
+        self.stdscr.nodelay(False)
         choice = 0
 
         while True:
@@ -48,8 +51,38 @@ class CMD_line_interface:
    
         return choice
 
+    def get_config_filepath(self):
+        self.stdscr.clear()
+
+        msg = "Enter path to configuration file: (hit Ctrl-G to send)"
+        self.stdscr.addstr(0, 0, msg)
+
+        editwin = curses.newwin(3,len(msg), 2,1)
+        rectangle(self.stdscr, 1,0, 1+3+1, 1+len(msg)+1)
+        self.stdscr.refresh()
+
+        box = Textbox(editwin)
+
+        # Let the user edit until Ctrl-G is struck.
+        box.edit()
+
+        # Get resulting contents
+        path = box.gather()
+
+        return path
+
+    def p_in_box_input_page(self):
+        self.stdscr.clear()
+
+
+    def display_temp_msg(self,msg):
+        self.stdscr.clear()
+        self.stdscr.addstr(1,5, msg)
+        self.stdscr.refresh()
+
 
     def close_application(self):
+        curses.nocbreak()
         self.stdscr.keypad(False)
         curses.echo()
         curses.endwin()
