ae750576b04aa437130c840f3841622c5d091cc9
Ellie Heywood <ejh3g21@soton.ac.uk>
2024-04-08 18:08:15 +0100

    WIP: generating a script to build a circuit to prepare the state of qubits

--- model/simulations/state_preparation.py
+++ model/simulations/state_preparation.py
@@ -1,37 +1,66 @@
 import numpy as np
 import cirq
 
-def calculate_angles(a):
+def calc_angle(s,j,amps):
     
-    n = np.log2(len(a))
-    angles = np.zeros((n,2**(n-1)))
+    numerator = 0
+    #print("Calculating numerator...")
+    for l in range(1, int(2 ** (s-1))+1):
+        index = int(((2*j) - 1)*(2**(s-1)) + l) -1
+        amp = np.abs(amps[index]) ** 2
+        #print(l, ": ", amp)
+        numerator += amp
+
+    numerator = np.sqrt(numerator)
+    #print("Numerator: ", numerator)
     
-    K = np.linspace(1,n,n,dtype=int)
+    denominator = 0
+    #print("Calculating denom...")
+    for l in range(1, int(2 ** s) +1):
+        index = int((j - 1)*(2**s) + l) -1
+        amp = np.abs(amps[index]) ** 2
+        #print(l, ": ", amp)
+        denominator += amp
 
-    for k in K:
-        print("k: ", k)
-        stop = int(2 ** (n-k))
-        J = np.linspace(1,stop,stop,dtype=int)
-        for j in J:
-            print("j: ", j)
-            numerator = 0
-            for l in range(int(2 ** (k-1))):
-                index = int(((2*j)-1)*(2**(k-1))+l) -1
-                numerator += (a[index] ** 2)
+    denominator = np.sqrt(denominator)
+    #print("Denominator: ", denominator)
+    return 2 * np.arcsin(numerator / denominator)
 
-            denominator = 0
-            for l in range(int(2 ** k)):
-                index = int((j-1)*(2**k)+l) - 1
-                denominator += (a[index] ** 2)
-
-            frac = np.sqrt(numerator) / np.sqrt(denominator)
-
-            print(2 * np.arcsin(frac))
-            angles[k-1][j-1] = 2 * np.arcsin(frac)
-            
-    return angles
-
-def state_prep(amps) -> cirq.Moment:
+def state_prep(amps,qubits) -> cirq.Moment:
     ## amps must be in order from 000, 001, ... 110, 111
+    ## take in prop distrib- add up to 1
+    ## must normalise by applying np.sqrt on all
 
-    angles = calculate_angles(a)
+    n = len(qubits)
+
+    ## Normalise amps
+    if np.sum(amps) != 1:
+        raise Exception("Probability distribution must add to 1")
+
+    amps = np.sqrt(amps)
+    print(amps)
+
+    moment = []
+    for s in range(n):
+        j = 1
+        for j in range(int(2**(n-s)),0,-1):
+            # no control qubit:
+            beta = calc_angle(s,j,amps)
+            if beta.isnan():
+                beta = 0
+            if s == 1:
+                moment.append(cirq.Moment(cirq.Ry(rads=beta).on(qubits[s])))
+            else:
+                control_order = list(bin(2**(n-s) - j + 1))[2:]
+                moment.append(cirq.Moment(cirq.Ry(rads=beta).on(qubits[s]).controlled_by(qubits[1])))
+            j += 1
+
+    return moment
+                
+
+
+if __name__ == '__main__':
+    qubits = cirq.LineQubit.range(2)
+    a = [0,0.5,0.5,0]
+    moment = state_prep(a,qubits)
+    print(cirq.Circuit(moment).to_text_diagram())
